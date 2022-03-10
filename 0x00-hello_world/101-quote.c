a8988551a4e6f7bbbf79c39d4fc99edb77bb75be
KKgaincay <abraham.alemu.1990@gmail.com>
2022-03-10 13:30:31 -0800

    Expect text is printed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n", n);
+
+	return (1);
+}
