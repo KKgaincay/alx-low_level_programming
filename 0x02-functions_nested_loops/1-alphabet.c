eef2faa8ad91542be9cd6580709587f07cf0e622
KKgaincay <abraham.alemu.1990@gmail.com>
2022-03-15 13:30:10 -0700

    All alphabets are in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entering point
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
